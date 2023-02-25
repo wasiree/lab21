0756ab843388d8af91340d43119b91a4a9141a4b
wasiree <wasiree098@gmail.com>
2023-02-25 21:42:19 +0700

    ok

--- lab21.h
+++ lab21.h
@@ -65,10 +65,38 @@ void Unit::newTurn(){
 
 
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+bool Unit::isDead(){
+	if(hp <= 0 ) return true;
+	else return false;
+}
 
+void Unit::guard(){
+    guard_on = true;
+}
+
+int Unit::beAttacked(int oppatk){
+    int dmg = oppatk-def;
+    if(dmg < 0) dmg = 0;
+    if(guard_on == true) dmg = dmg/3;
+    hp = hp-dmg;
+    return dmg;
+}
+
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
+
+int Unit::heal(){
+	int hphero = rand()%21+10;
+	if(hp + hphero >= hpmax){
+		hphero = hpmax - hp;
+		hp = hpmax;
+ 	}else{
+		hp = hp + hphero;
+	}
+	return hphero;
+}
+    
 
 
 void drawScene(char p_action,int p,char m_action,int m){
